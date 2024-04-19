fb3c1d239d09837516d5d3c2abb62e887a12d26f
VefxoNadiradze <olld00@bk.ru>
2024-04-19 22:28:37 +0400

    add responsive

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,19 +1,35 @@
 import styled from "styled-components";
 import Datas from "../data.json";
 import { Link } from "react-router-dom";
+import burgerImage from "/assets/icon-hamburger.svg";
+import { useState } from "react";
 
 export default function Header() {
+  let [headerActive, setheaderActive] = useState<boolean>(false);
   return (
-    <HeaderComponent>
-      <Link to={"/Page/Earth"} className="Logo">
-        THE PLANETS
-      </Link>
+    <HeaderComponent headerActive={headerActive}>
+      <div className="logo-burger">
+        <Link to={"/Page/Earth"} className="Logo">
+          THE PLANETS
+        </Link>
+
+        <button
+          className="burger"
+          onClick={() => setheaderActive(!headerActive)}
+        >
+          <img src={burgerImage} alt="burger image" />
+        </button>
+      </div>
 
       <ul>
         {Datas.map((dataItem, index) => {
           return (
             <li key={index}>
-              <Link to={`/Page/${dataItem.name}`} className="navItems">
+              <Link
+                to={`/Page/${dataItem.name}`}
+                className="navItems"
+                onClick={() => setheaderActive(false)}
+              >
                 {dataItem.name}
               </Link>
             </li>
@@ -24,12 +40,27 @@ export default function Header() {
   );
 }
 
-const HeaderComponent = styled.header`
+const HeaderComponent = styled.header<{ headerActive: boolean }>`
   padding: 22px 32px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 1px solid #393850;
+
+  @media screen and (max-width: 970px) {
+    row-gap: 39px;
+    flex-direction: column;
+  }
+
+  @media screen and (max-width: 660px) {
+    align-items: flex-start;
+    position: fixed;
+    z-index: 10;
+    padding: 0;
+    width: 100%;
+    gap: 0;
+    background: rgb(7, 7, 36);
+  }
 
   .Logo {
     text-decoration: none;
@@ -42,11 +73,53 @@ const HeaderComponent = styled.header`
     text-transform: uppercase;
   }
 
+  .burger {
+    display: none;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    width: 24px;
+    height: 17px;
+  }
+
+  @media screen and (max-width: 660px) {
+    .logo-burger {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 16px 24px;
+      width: 100%;
+    }
+
+    .burger {
+      display: block;
+    }
+  }
+
   ul {
     list-style: none;
     display: flex;
     align-items: center;
     column-gap: 32px;
+    transition: 1s ease-in-out;
+
+    @media screen and (max-width: 660px) {
+      position: fixed;
+      transform: ${(props) =>
+        props.headerActive ? "translateX(0%)" : "translateX(-300%)"};
+      top: 68px;
+      flex-direction: column;
+      align-items: flex-start;
+      min-height: 100vh;
+      width: 100%;
+      background: rgb(7, 7, 36);
+
+      li {
+        width: 100%;
+        padding: 22px 32px 22px 24px;
+        border-bottom: 1px solid #393850;
+      }
+    }
 
     li {
       .navItems {
--- src/Components/Planets.tsx
+++ src/Components/Planets.tsx
@@ -44,6 +44,35 @@ export default function Planets() {
           planetName={Planets.name}
         >
           <motion.div
+            className="MobileButtonsParent"
+            initial={{
+              opacity: 0,
+
+              transformStyle: "preserve-3d",
+              transform: "perspective(1000px) rotateY(80deg) translateX(30px)",
+            }}
+            animate={{
+              opacity: 1,
+
+              transformStyle: "preserve-3d",
+              transform: "perspective(1000px) rotateY(0deg)  translateX(0px)",
+            }}
+            transition={{ duration: 1.1 }}
+          >
+            <button onClick={(e) => HandleDescription(e)}>
+              <span>01</span>OVERVIEW
+            </button>
+
+            <button onClick={(e) => HandleDescription(e)}>
+              <span>02</span> Internal Structure
+            </button>
+            <button onClick={(e) => HandleDescription(e)}>
+              <span>03</span>
+              Surface Geology
+            </button>
+          </motion.div>
+
+          <motion.div
             initial={{
               opacity: 0,
 
@@ -79,53 +108,55 @@ export default function Planets() {
               transition={{ duration: 1.1 }}
             >
               <h2 className="planetName">{Planets.name}</h2>
-            </motion.div>
 
-            <motion.div
-              initial={{
-                opacity: 0,
+              <motion.div
+                initial={{
+                  opacity: 0,
 
-                transformStyle: "preserve-3d",
-                transform:
-                  "perspective(1000px) rotateY(80deg) translateX(150px)",
-              }}
-              animate={{
-                opacity: 1,
+                  transformStyle: "preserve-3d",
+                  transform:
+                    "perspective(1000px) rotateY(80deg) translateX(150px)",
+                }}
+                animate={{
+                  opacity: 1,
 
-                transformStyle: "preserve-3d",
-                transform: "perspective(1000px) rotateY(0deg)  translateY(0px)",
-              }}
-              transition={{ duration: 1.1 }}
-            >
-              <p className="Description">
-                {overview && Planets.overview.content}
-                {structure && Planets.structure.content}
-                {geology && Planets.geology.content}
-              </p>
-            </motion.div>
+                  transformStyle: "preserve-3d",
+                  transform:
+                    "perspective(1000px) rotateY(0deg)  translateY(0px)",
+                }}
+                transition={{ duration: 1.1 }}
+              >
+                <p className="Description">
+                  {overview && Planets.overview.content}
+                  {structure && Planets.structure.content}
+                  {geology && Planets.geology.content}
+                </p>
+              </motion.div>
 
-            <motion.div
-              className="sourceParent"
-              initial={{
-                opacity: 0,
+              <motion.div
+                className="sourceParent"
+                initial={{
+                  opacity: 0,
 
-                transformStyle: "preserve-3d",
-                transform:
-                  "perspective(1000px) rotateX(80deg) translateY(30px)",
-              }}
-              animate={{
-                opacity: 1,
+                  transformStyle: "preserve-3d",
+                  transform:
+                    "perspective(1000px) rotateX(80deg) translateY(30px)",
+                }}
+                animate={{
+                  opacity: 1,
 
-                transformStyle: "preserve-3d",
-                transform: "perspective(1000px) rotateX(0deg)  translateY(0px)",
-              }}
-              transition={{ duration: 1.1 }}
-            >
-              Source:
-              <a href={Planets.overview.source} className="source">
-                Wikipedia
-                <img src={Sourceicon} alt="source icon" />
-              </a>
+                  transformStyle: "preserve-3d",
+                  transform:
+                    "perspective(1000px) rotateX(0deg)  translateY(0px)",
+                }}
+                transition={{ duration: 1.1 }}
+              >
+                Source:
+                <a href={Planets.overview.source} className="source">
+                  Wikipedia
+                  <img src={Sourceicon} alt="source icon" />
+                </a>
+              </motion.div>
             </motion.div>
 
             <motion.div
@@ -245,14 +276,71 @@ const PlanetImageAbout = styled.div<{
   margin-top: 96px;
   color: white;
 
+  .MobileButtonsParent {
+    display: none;
+
+    @media screen and (max-width: 660px) {
+      margin-bottom: 50px;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      border-bottom: 1px solid #393850;
+      width: 100%;
+      button {
+        border: none;
+        background-color: transparent;
+        padding: 20px 24px;
+        color: rgb(255, 255, 255);
+        font-size: 9px;
+        font-weight: 700;
+        line-height: 10px;
+        letter-spacing: 1.93px;
+        text-transform: uppercase;
+
+        span {
+          display: none;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 970px) {
+    flex-direction: column;
+  }
+
+  @media screen and (max-width: 660px) {
+    margin-top: 0;
+  }
+
   img {
     height: ${(props) => props.planetSizes.Desktop.height};
     width: ${(props) => props.planetSizes.Desktop.width};
+
+    @media screen and (max-width: 1032px) {
+      height: ${(props) => props.planetSizes.Tablet.height};
+      width: ${(props) => props.planetSizes.Tablet.width};
+    }
+
+    @media screen and (max-width: 500px) {
+      height: ${(props) => props.planetSizes.Mobile.height};
+      width: ${(props) => props.planetSizes.Mobile.width};
+    }
   }
 
   .AboutPlanet {
     display: flex;
     flex-direction: column;
+    @media screen and (max-width: 970px) {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+      width: 100%;
+    }
+
+    @media screen and (max-width: 660px) {
+      justify-content: center;
+      text-align: center;
+      margin-top: 98px;
+    }
 
     .planetName {
       color: rgb(255, 255, 255);
@@ -262,6 +350,14 @@ const PlanetImageAbout = styled.div<{
       line-height: 104px;
       text-transform: uppercase;
       margin-bottom: 23px;
+
+      @media screen and (max-width: 970px) {
+        font-size: 48px;
+      }
+
+      @media screen and (max-width: 660px) {
+        margin-bottom: 19px;
+      }
     }
 
     .Description {
@@ -271,6 +367,10 @@ const PlanetImageAbout = styled.div<{
       font-weight: 400;
       line-height: 25px;
       margin-bottom: 23px;
+
+      @media screen and (max-width: 970px) {
+        font-size: 11px;
+      }
     }
 
     .sourceParent {
@@ -285,11 +385,18 @@ const PlanetImageAbout = styled.div<{
         line-height: 25px;
         margin-left: 10px;
 
+        @media screen and (max-width: 970px) {
+          font-size: 12px;
+        }
+
         img {
           width: 12px;
           height: 12px;
           margin-left: 15px;
         }
+      }
+      @media screen and (max-width: 660px) {
+        justify-content: center;
       }
     }
 
@@ -313,9 +420,23 @@ const PlanetImageAbout = styled.div<{
         box-sizing: border-box;
         border: 1px solid #393850;
 
+        @media screen and (max-width: 970px) {
+          font-size: 9px;
+          letter-spacing: 1.93px;
+          padding: 8px 20px;
+        }
+
+        @media screen and (max-width: 660px) {
+          display: none;
+        }
+
         span {
           opacity: 0.5;
           margin-right: 28px;
+
+          @media screen and (max-width: 970px) {
+            margin-right: 15px;
+          }
         }
       }
     }
@@ -335,6 +456,11 @@ const PlanetsFooter = styled.div`
     box-sizing: border-box;
     border: 1px solid #2f2f47;
 
+    @media screen and (max-width: 970px) {
+      padding: 16px 15px 19px 15px;
+      min-width: 164px;
+    }
+
     p {
       color: rgb(255, 255, 255);
       font-size: 11px;
@@ -343,6 +469,10 @@ const PlanetsFooter = styled.div`
       letter-spacing: 1px;
       text-transform: uppercase;
       opacity: 0.5;
+
+      @media screen and (max-width: 970px) {
+        font-size: 8px;
+      }
     }
 
     h4 {
@@ -353,6 +483,9 @@ const PlanetsFooter = styled.div`
       line-height: 52px;
       letter-spacing: -1.5px;
       text-transform: uppercase;
+      @media screen and (max-width: 970px) {
+        font-size: 24px;
+      }
     }
   }
 `;
